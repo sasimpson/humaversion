4f5bc8aaf8a1950b7c75fbb02bec3e5b03f21c95
Scott Simpson <sasimpson@gmail.com>
2025-04-09 21:36:43 -0500

    nope i'm dumb :)

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/gorilla/mux"
 	"humaversion/service"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -24,23 +29,22 @@ func main() {
 	}
 
 	log.Println("starting http server...")
-	//go func() {
-	//	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-	//		log.Fatalf("http server error: %v\n", err)
-	//	}
-	//	log.Println("stopped serving new connections.")
-	//}()
-	//
-	//sigChan := make(chan os.Signal, 1)
-	//signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	//<-sigChan
-	//
-	//shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 5*time.Second)
-	//defer shutdownRelease()
-	//
-	//if err := server.Shutdown(shutdownCtx); err != nil {
-	//	log.Fatalf("http server shutdown error: %v\n", err)
-	//}
-	//log.Println("graceful http server shutdown.")
-	server.ListenAndServe()
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("http server error: %v\n", err)
+		}
+		log.Println("stopped serving new connections.")
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("http server shutdown error: %v\n", err)
+	}
+	log.Println("graceful http server shutdown.")
 }
